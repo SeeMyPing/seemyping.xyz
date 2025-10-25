0b71b583820b63c75e866832643e3d10e44be927
Nawer KEZAL <nawer@MacBookPro.lan>
2025-10-25 21:42:55 +0200

    Add template

--- src/seemyping/settings.py
+++ src/seemyping/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'livereload',
     'django.contrib.staticfiles',
+
     'posts',
 ]
 
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript'
 ]
 
 ROOT_URLCONF = 'seemyping.urls'
